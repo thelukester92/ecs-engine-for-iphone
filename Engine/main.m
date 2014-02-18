90f77ceddf01d536f2fbf61f034b844cab0ea5b6
Luke Godfrey <thelukester92@me.com>
2014-02-18 13:04:03 -0600

    Initial Commit

---
+++ Engine/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Engine
+//
+//  Created by Luke Godfrey on 2/18/14.
+//  Copyright (c) 2014 Luke Godfrey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
+}
